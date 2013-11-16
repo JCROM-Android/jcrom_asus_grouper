a56090bce7e844db61975c9b396d3727e604bcba
Akihiro Maeda <sola.1980.a@gmail.com>
2013-11-17 01:29:46 +0900

    Changed to use the binary for Android 4.4.

--- device-common.mk
+++ device-common.mk
@@ -1,13 +1,16 @@
 LOCAL_PATH := jcrom/asus/grouper
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Girtab.ogg \
+    ro.config.notification_sound=Tejat.ogg \
+    ro.config.alarm_alert=Cesium.ogg \
     ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.error.receiver.system.apps=com.google.android.feedback \
+    ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
     keyguard.no_require_sim=true \
     ro.facelock.black_timeout=1250 \
@@ -18,7 +21,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.facelock.use_intro_anim=true \
     ro.media.effect.bgdropper.adj=0.2 \
     camera.flash_off=0 \
-    ro.setupwizard.gservices_wait=true \
     ro.com.widevine.cachesize=16777216 \
-    ro.setupwizard.wifi_required=true
+    media.aac_51_output_enabled=true \
+    ro.setupwizard.network_required=true
 
--- proprietary/extract-files.sh
+++ proprietary/extract-files.sh
@@ -1,66 +1,36 @@
 #!/bin/bash
 echo "Download and Deodexing... Please wait."
-wget -nc -q https://dl.google.com/dl/android/aosp/nakasi-jwr66y-factory-1e503532.tgz
-tar zxf nakasi-jwr66y-factory-1e503532.tgz
-cd nakasi-jwr66y
-unzip image-nakasi-jwr66y.zip
+wget -nc -q https://dl.google.com/dl/android/aosp/nakasi-krt16o-factory-db4a1a8a.tgz
+tar zxf nakasi-krt16o-factory-db4a1a8a.tgz
+cd nakasi-krt16o
+unzip image-nakasi-krt16o.zip
 cd ../
-./simg2img nakasi-jwr66y/system.img system.ext4.img
+./simg2img nakasi-krt16o/system.img system.ext4.img
 mkdir system
 mkdir tmp
 sudo mount -o loop -t ext4 system.ext4.img tmp
 sync
+mkdir -p system/bin
+mkdir -p system/etc
 mkdir -p system/lib/soundfx
-mkdir -p system/vendor/lib/drm
+mkdir -p system/media
 mkdir -p system/vendor/media
 mkdir -p system/vendor/lib/mediadrm
 mkdir -p system/vendor/etc
-mkdir -p system/media/video
-mkdir -p system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7
-mkdir -p system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6
-mkdir -p system/vendor/pittpatt/models/recognition
 
-cp -a tmp/vendor/lib/drm/libdrmwvmplugin.so system/vendor/lib/drm/libdrmwvmplugin.so
-cp -a tmp/vendor/lib/libWVStreamControlAPI_L1.so system/vendor/lib/libWVStreamControlAPI_L1.so
-cp -a tmp/vendor/lib/libwvdrm_L1.so system/vendor/lib/libwvdrm_L1.so
-cp -a tmp/vendor/lib/libwvm.so system/vendor/lib/libwvm.so
-cp -a tmp/media/video/AndroidInSpace.240p.mp4 system/media/video/AndroidInSpace.240p.mp4
-cp -a tmp/media/video/AndroidInSpace.480p.mp4 system/media/video/AndroidInSpace.480p.mp4
-cp -a tmp/media/video/Sunset.240p.mp4 system/media/video/Sunset.240p.mp4
-cp -a tmp/media/video/Sunset.480p.mp4 system/media/video/Sunset.480p.mp4
-cp -a tmp/media/bootanimation.zip system/media/bootanimation.zip
-cp -a tmp/lib/soundfx/libfmas.so system/lib/soundfx/libfmas.so
-cp -a tmp/lib/libRSSupport.so system/lib/libRSSupport.so
-cp -a tmp/lib/libgames_rtmp_jni.so system/lib/libgames_rtmp_jni.so
-cp -a tmp/lib/librs.antblur.so system/lib/librs.antblur.so
-cp -a tmp/lib/librs.antblur_constant.so system/lib/librs.antblur_constant.so
-cp -a tmp/lib/librs.antblur_drama.so system/lib/librs.antblur_drama.so
-cp -a tmp/lib/librs.drama.so system/lib/librs.drama.so
-cp -a tmp/lib/librs.film_base.so system/lib/librs.film_base.so
-cp -a tmp/lib/librs.fixedframe.so system/lib/librs.fixedframe.so
-cp -a tmp/lib/librs.grey.so system/lib/librs.grey.so
-cp -a tmp/lib/librs.image_wrapper.so system/lib/librs.image_wrapper.so
-cp -a tmp/lib/librs.retrolux.so system/lib/librs.retrolux.so
-cp -a tmp/lib/librsjni.so system/lib/librsjni.so
-cp -a tmp/lib/libvcdecoder_jni.so system/lib/libvcdecoder_jni.so
-cp -a tmp/lib/libvideochat_jni.so system/lib/libvideochat_jni.so
-cp -a tmp/lib/libwebp_android.so system/lib/libwebp_android.so
-cp -a tmp/lib/libwebrtc_audio_coding.so system/lib/libwebrtc_audio_coding.so
-cp -a tmp/vendor/pittpatt/models/recognition/face.face.y0-y0-22-b-N.bin system/vendor/pittpatt/models/recognition/face.face.y0-y0-22-b-N.bin
-cp -a tmp/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rn30-ri30.5-v24.bin system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rn30-ri30.5-v24.bin
-cp -a tmp/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rp30-ri30.5-v24.bin system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-rp30-ri30.5-v24.bin
-cp -a tmp/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-r0-ri30.4a-v24.bin system/vendor/pittpatt/models/detection/yaw_roll_face_detectors.6/head-y0-yi45-p0-pi45-r0-ri30.4a-v24.bin
-cp -a tmp/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32.bin system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/left_eye-y0-yi45-p0-pi45-r0-ri20.lg_32.bin
-cp -a tmp/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/right_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-2.bin system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/right_eye-y0-yi45-p0-pi45-r0-ri20.lg_32-2.bin
-cp -a tmp/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/nose_base-y0-yi45-p0-pi45-r0-ri20.lg_32.bin system/vendor/pittpatt/models/detection/multi_pose_face_landmark_detectors.7/nose_base-y0-yi45-p0-pi45-r0-ri20.lg_32.bin
-cp -a tmp/vendor/media/LMspeed_508.emd system/vendor/media/LMspeed_508.emd
-cp -a tmp/vendor/media/PFFprec_600.emd system/vendor/media/PFFprec_600.emd
+cp -a tmp/vendor/etc/audio_effects.conf system/vendor/etc/audio_effects.conf
 cp -a tmp/vendor/lib/mediadrm/libwvdrmengine.so system/vendor/lib/mediadrm/libwvdrmengine.so
 cp -a tmp/vendor/lib/libfrsdk.so system/vendor/lib/libfrsdk.so
-cp -a tmp/vendor/etc/audio_effects.conf system/vendor/etc/audio_effects.conf
+cp -a tmp/vendor/media/LMspeed_508.emd system/vendor/media/LMspeed_508.emd
+cp -a tmp/vendor/media/PFFprec_600.emd system/vendor/media/PFFprec_600.emd
+cp -a tmp/bin/tf_daemon system/bin/tf_daemon
+cp -a tmp/etc/audio_effects.conf system/etc/audio_effects.conf
+cp -a tmp/etc/apns-conf.xml system/etc/apns-conf.xml
+cp -a tmp/lib/soundfx/libfmas.so system/lib/soundfx/libfmas.so
+cp -a tmp/media/bootanimation.zip system/media/bootanimation.zip
 
 sudo umount tmp
 rm -rf tmp
-rm -rf nakasi-jwr66y
+rm -rf nakasi-krt16o
 rm system.ext4.img
 
